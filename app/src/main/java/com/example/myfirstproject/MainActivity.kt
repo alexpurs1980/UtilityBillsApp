085a6423d2fbaf959ec0d82e7be1165ec7d3eb42
Aleksey <alexpurs@yandex.ru>
2022-12-20 14:03:28 +0300

    Add date of calc and date for tariffs, change all nums in calc on values (vars)

--- app/src/main/java/com/example/myfirstproject/MainActivity.kt
+++ app/src/main/java/com/example/myfirstproject/MainActivity.kt
@@ -40,23 +40,34 @@ class MainActivity : AppCompatActivity() {
             val coldWaterLast = binding.coldWaterLastEditText.text.toString().toInt()
             val coldWaterNow = binding.coldWaterNowEditText.text.toString().toInt()
             val gasPrice = binding.gasEditText.text.toString().toInt()
+            var dateOfCalc = binding.editTextTextDateOfCalc?.text.toString()
 
-            //вычисление (работа с данными) - тарифы на 1 июля 2022 г.
-            // кВт час = 6,17 руб.
-            // горячая вода = 223,04 руб.
-            // холодная вода = 45,88 руб.
-            // газ = 60 руб.
-            val electricySum = "Электричество ${electricyNow - electricyLast} кВт х 6,17 руб. = ${(electricyNow - electricyLast) * 6.17} руб."
-            val hotWaterSum = "\nГорячая вода ${hotWaterNow - hotWaterLast} куб.м х 223,04 руб. = ${(hotWaterNow - hotWaterLast) * 223.04} руб."
-            val coldWaterSum = "\nХолодная вода ${coldWaterNow - coldWaterLast} куб.м х 45,88 руб. = ${(coldWaterNow - coldWaterLast) * 45.88} руб."
+//            // TARIFFS 01/06/2022
+//            val kWh = 6.11
+//            val hotCubeMeter = 223.04
+//            val coldCubeMeter = 45.88
+//            val gasFixPrice = gasPrice
+
+            // TARIFFS 01/12/2022
+            val kWh = 6.43
+            val hotCubeMeter = 243.16
+            val coldCubeMeter = 50.93
+            val gasFixPrice = gasPrice
+
+
+            val electricySum = "\nЭлектричество ${electricyNow - electricyLast} кВт х ${kWh} руб. = ${(electricyNow - electricyLast) * kWh} руб."
+            val hotWaterSum = "\nГорячая вода ${hotWaterNow - hotWaterLast} куб.м х ${hotCubeMeter} руб. = ${(hotWaterNow - hotWaterLast) * hotCubeMeter} руб."
+            val coldWaterSum = "\nХолодная вода ${coldWaterNow - coldWaterLast} куб.м х ${coldCubeMeter} руб. = ${(coldWaterNow - coldWaterLast) * coldCubeMeter} руб."
             val gasSum = "\nГаз ${gasPrice} руб."
-            val totalSum = "\nИтого за месяц ${((electricyNow - electricyLast) * 6.17) + ((hotWaterNow - hotWaterLast) * 223.04) + ((coldWaterNow - coldWaterLast) * 45.88) + 60} руб."
-            val dateToPay = "Тарифы по состоянию на 1 июля 2022 г."
+            val totalSum = "\nИтого за месяц ${((electricyNow - electricyLast) * kWh) + ((hotWaterNow - hotWaterLast) * hotCubeMeter) + ((coldWaterNow - coldWaterLast) * coldCubeMeter) + gasFixPrice} руб."
+//            val dateToPay = "\nТарифы по состоянию на 1 июля 2022 г."
+            val dateToPay = "\nТарифы по состоянию на 1 декабря 2022 г."
+            dateOfCalc = "Дата расчета: " + dateOfCalc
 
 
             //выводим результат (поле принимает только строку - перевод в Стринг)
             binding.resultTextView.setVisibility(View.VISIBLE)
-            binding.resultTextView.text = electricySum + hotWaterSum + coldWaterSum + gasSum + totalSum
+            binding.resultTextView.text = dateOfCalc + dateToPay+ electricySum + hotWaterSum + coldWaterSum + gasSum + totalSum
 
         }
 
